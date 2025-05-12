ec2b81481bf6dd4e01c31e9d9f103e4c4bc231ae
IMasterChiefI <h.kleibeler@web.de>
2025-05-12 11:22:02 +0200

    Create OpenApiAppApplication.java

---
+++ open-api/open-api-app/src/main/java/app/OpenApiAppApplication.java
@@ -0,0 +1,11 @@
+package com.github.skjolber.packing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiAppApplication.class, args);
+    }
+}
